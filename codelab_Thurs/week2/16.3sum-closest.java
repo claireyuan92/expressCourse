a262f7f5ae23a9f6aabb7e5302409e78159c2591
Claire Yuan <claire.yuan@duke.edu>
2017-06-13 20:36:35 -0700

    16 and advancedDS notes

--- codelab_Thurs/week2/16.3sum-closest.java
+++ codelab_Thurs/week2/16.3sum-closest.java
@@ -1,5 +1,26 @@
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        
+	Arrays.sort(nums);
+	int res = -1;
+	int min_distance = Integer.MAX_VALUE;	
+	for(int i = 0; i < nums.length ; i++) {
+	    int left = i + 1;
+	    int right = nums.length -1;
+	    while(left < right ) {
+		int sum =nums[i] + nums[left] + nums[right];
+		int current_distance  = Math.abs(target - sum);
+		if (current_distance < min_distance ) {
+		    min_distance = current_distance;
+		    res =sum;
+		 
+		}
+		if(sum  < target) {
+		    left++;
+		} else {
+		    right--;
+		}
+	    }
+	}
+	return res;
     }
 }
