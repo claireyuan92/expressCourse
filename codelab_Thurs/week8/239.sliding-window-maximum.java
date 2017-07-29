62dbc59bd747e575d63b3ed0100315ec46ff7483
Claire Yuan <claire.yuan@duke.edu>
2017-07-29 15:26:29 -0700

    add tests

---
+++ codelab_Thurs/week8/239.sliding-window-maximum.java
@@ -0,0 +1,7 @@
+public class Solution {
+
+    
+    public int[] maxSlidingWindow(int[] nums, int k) {
+	
+    }
+}
---
+++ codelab_Thurs/week8/test.sh
@@ -0,0 +1,7 @@
+declare -a arr=(3 402 282 84 422 316)
+for i in "${arr[@]}"
+do
+
+    leetcode  show "$i" --gen -l java
+done
+
